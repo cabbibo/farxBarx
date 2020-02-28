0468592d6908009993c8e717f3a31e858748905d
cabbibo <isaaclandoncohen@gmail.com>
2020-02-29 08:55:55 +1100

    first

---
+++ Assets/GetQuestInputs.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class GetQuestInputs : MonoBehaviour
+{
+
+
+    public float leftValue;
+    public float rightValue;
+    public TextMesh leftVal;
+    public TextMesh rightVal;
+
+    public Transform leftLocation;
+    public Transform rightLocation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      OVRInput.Update();
+        
+    }
+
+    void FixedUpdate(){
+      OVRInput.FixedUpdate();
+
+      // returns a float of the secondary (typically the Right) index finger trigger’s current state.
+// (range of 0.0f to 1.0f)
+      rightValue = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+      leftValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+
+      leftVal.text = " left : " + leftValue;
+      rightVal.text = " right : " + rightValue;
+
+    }
+}
---
+++ Assets/pullGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pullGame : MonoBehaviour
+{
+
+    public Rigidbody ball;
+    public GetQuestInputs inputs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        ball.AddForce(-(ball.transform.position - inputs.rightLocation.position) * inputs.rightValue);
+    }
+}
